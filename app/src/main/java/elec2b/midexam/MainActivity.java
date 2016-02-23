df4fbeec2c464117de941b30bd41b1c0f29d8582
nuTShELLS <claritacartilla@gmail.com>
2016-02-23 18:06:53 +0800

    First Commit

---
+++ app/src/main/java/elec2b/midexam/MainActivity.java
@@ -0,0 +1,65 @@
+package elec2b.midexam;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Book book = new Book();
+    JSONArray books = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "God Bless!", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.getJSONObject("b1", book);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/elec2b/midexam/ServiceHandler.java
@@ -0,0 +1,23 @@
+package elec2b.midexam;
+
+/**
+ * Created by LENOVO on 2/23/2016.
+ */
+public class ServiceHandler {
+
+    static String resP = null;
+    private final static int GET = 1;
+    private final static int POST = 2;
+
+
+    public ServiceHandler(){
+
+    }
+
+    private String makeServiceCall(String url, int method){
+        return this.makeServiceCall(url, method);
+    }
+
+    private String makeServiceCall(String url, int method, List<>)
+
+}
